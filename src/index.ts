9110f1c6206d970ff94e6db11c24c7bf7dda6ea4
Stas Gatilov <stasgat12@gmail.com>
2022-12-17 00:59:57 +0300

    feat: add Card instance

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import './global.scss';
 import { Footer } from './modules/store-page/components/footer/footer';
 import { Header } from './modules/store-page/components/header/header';
 import { Main } from './modules/store-page/components/main/main';
+import { Card } from './modules/store-page/components/cards/cards';
 
 new Header().create();
 new Main().create();
 new Footer().create();
+new Card().createCatalog();

ac5698c34797646fa5edb06b1cfdc86ca030a915
Stas Gatilov <stasgat12@gmail.com>
2022-12-18 19:36:29 +0300

    feat: add url.ts

---
+++ src/pages/main/url.ts
@@ -0,0 +1,23 @@
+// Вынес этот файл наружу, чтобы можно было добавлять query-запросы от любых фильтров главной страницы
+
+class UrlPath {
+  params: URLSearchParams;
+  constructor() {
+    this.params = new URLSearchParams(window.location.search);
+  }
+  setQuery() {
+    const sortOptions: HTMLSelectElement | null = document.querySelector('.selector');
+
+    if (sortOptions) {
+      sortOptions.addEventListener('change', (): void => {
+        if (this.params.has('sort')) {
+          this.params.delete('sort');
+        }
+        this.params.set('sort', sortOptions.selectedOptions[0].value);
+        window.history.pushState({}, '', `?${this.params.toString()}`);
+      });
+    }
+  }
+}
+
+export { UrlPath };

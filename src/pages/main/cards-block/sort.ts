80bc0af43a6feb8a0529ea8dd04ecdfc4f7731aa
Stas Gatilov <stasgat12@gmail.com>
2022-12-21 21:20:53 +0300

    fix: make correct display of selected option of "select" element after page update

--- src/pages/main/cards-block/sort.ts
+++ src/pages/main/cards-block/sort.ts
@@ -7,33 +7,54 @@ class CardsSort extends CardsBlock {
 
     this.sortByPriceAsc();
     this.sortByPriceDesc();
-    this.sortByDiscountAsc();
-    this.sortByDiscountDesc();
     this.sortByRatingAsc();
     this.sortByRatingDesc();
+    this.sortByDiscountAsc();
+    this.sortByDiscountDesc();
+    this.handleSelectedAttribute();
 
     if (sortOptions) {
       sortOptions.addEventListener('change', (): void => {
         this.sortByPriceAsc();
         this.sortByPriceDesc();
-        this.sortByDiscountAsc();
-        this.sortByDiscountDesc();
         this.sortByRatingAsc();
         this.sortByRatingDesc();
+        this.sortByDiscountAsc();
+        this.sortByDiscountDesc();
       });
     }
   }
 
-  setOptionSelected(numberOfOption: number): void {
+  handleSelectedAttribute(): void {
     const sortOptions: HTMLSelectElement | null = document.querySelector('.selector');
-    sortOptions?.options[numberOfOption].setAttribute('selected', '');
+    if (sortOptions) {
+      switch (true) {
+        case window.location.search === '?sort=price-ASC':
+          sortOptions.options[1].setAttribute('selected', '');
+          break;
+        case window.location.search === '?sort=price-DESC':
+          sortOptions.options[2].setAttribute('selected', '');
+          break;
+        case window.location.search === '?sort=rating-ASC':
+          sortOptions.options[3].setAttribute('selected', '');
+          break;
+        case window.location.search === '?sort=rating-DESC':
+          sortOptions.options[4].setAttribute('selected', '');
+          break;
+        case window.location.search === '?sort=discount-ASC':
+          sortOptions.options[5].setAttribute('selected', '');
+          break;
+        case window.location.search === '?sort=discount-DESC':
+          sortOptions.options[6].setAttribute('selected', '');
+          break;
+      }
+    }
   }
 
   sortByPriceAsc(): void {
     if (window.location.search === '?sort=price-ASC') {
       onlineStoreData.products.sort((a, b) => a.price - b.price);
       super.updateCatalog();
-      this.setOptionSelected(1);
     }
   }
 
@@ -41,7 +62,6 @@ class CardsSort extends CardsBlock {
     if (window.location.search === '?sort=price-DESC') {
       onlineStoreData.products.sort((a, b) => b.price - a.price);
       super.updateCatalog();
-      this.setOptionSelected(2);
     }
   }
 
@@ -49,7 +69,6 @@ class CardsSort extends CardsBlock {
     if (window.location.search === '?sort=discount-ASC') {
       onlineStoreData.products.sort((a, b) => a.discountPercentage - b.discountPercentage);
       super.updateCatalog();
-      this.setOptionSelected(3);
     }
   }
 
@@ -57,7 +76,6 @@ class CardsSort extends CardsBlock {
     if (window.location.search === '?sort=discount-DESC') {
       onlineStoreData.products.sort((a, b) => b.discountPercentage - a.discountPercentage);
       super.updateCatalog();
-      this.setOptionSelected(4);
     }
   }
 
@@ -65,7 +83,6 @@ class CardsSort extends CardsBlock {
     if (window.location.search === '?sort=rating-ASC') {
       onlineStoreData.products.sort((a, b) => a.rating - b.rating);
       super.updateCatalog();
-      this.setOptionSelected(5);
     }
   }
 
@@ -73,7 +90,6 @@ class CardsSort extends CardsBlock {
     if (window.location.search === '?sort=rating-DESC') {
       onlineStoreData.products.sort((a, b) => b.rating - a.rating);
       super.updateCatalog();
-      this.setOptionSelected(6);
     }
   }
 }

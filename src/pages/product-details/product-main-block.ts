f1454a1cd1a406e91c88265d1eb071fdc8710def
MarinaZai <marintos34@gmail.com>
2022-12-24 14:59:52 +0100

    feat: add product page blocks

--- src/pages/product-details/product-main-block.ts
+++ src/pages/product-details/product-main-block.ts
@@ -81,8 +81,98 @@ export class ProductMain {
     productInfo.className = 'product-info';
     productData.appendChild(productInfo);
 
+    const productDescription = document.createElement('div');
+    productDescription.className = 'product-detail-item';
+    //productDescription.innerText = 'Description'
+    productInfo.appendChild(productDescription);
+    const productDescriptionTitle = document.createElement('h3');
+    productDescriptionTitle.className = 'product-item-title';
+    productDescriptionTitle.innerText = 'Description'
+    productDescription.appendChild(productDescriptionTitle);
+    const productDescriptionText = document.createElement('p');
+    productDescriptionText.className = 'product-item-text';
+    productDescriptionText.innerText = `${item?.description}`
+    productDescription.appendChild(productDescriptionText);
+
+    const productDiscount = document.createElement('div');
+    productDiscount.className = 'product-detail-item';
+   // productDiscount.innerText = 'Discount Percentage'
+    productInfo.appendChild(productDiscount);
+    const productDiscountTitle = document.createElement('h3');
+    productDiscountTitle.className = 'product-item-title';
+    productDiscountTitle.innerText = 'Discount Percentage'
+    productDiscount.appendChild(productDiscountTitle);
+    const productDiscountText = document.createElement('p');
+    productDiscountText.className = 'product-item-text';
+    productDiscountText.innerText = `${item?.discountPercentage}`
+    productDiscount.appendChild(productDiscountText);
+
+    const productRaiting = document.createElement('div');
+    productRaiting.className = 'product-detail-item';
+    //productRaiting.innerText = 'Raiting'
+    productInfo.appendChild(productRaiting);
+    const productRaitingTitle = document.createElement('h3');
+    productRaitingTitle.className = 'product-item-title';
+    productRaitingTitle.innerText = 'Raiting'
+    productRaiting.appendChild(productRaitingTitle);
+    const productRaitingText = document.createElement('p');
+    productRaitingText.className = 'product-item-text';
+    productRaitingText.innerText = `${item?.rating}`
+    productRaiting.appendChild(productRaitingText);
+
+    const productStock = document.createElement('div');
+    productStock.className = 'product-detail-item';
+    //productStock.innerText = 'Stock'
+    productInfo.appendChild(productStock);
+    const productStockTitle = document.createElement('h3');
+    productStockTitle.className = 'product-item-title';
+    productStockTitle.innerText = 'Stock'
+    productStock.appendChild(productStockTitle);
+    const productStockText = document.createElement('p');
+    productStockText.className = 'product-item-text';
+    productStockText.innerText = `${item?.stock}`
+    productStock.appendChild(productStockText);
+
+    const productBrand = document.createElement('div');
+    productBrand.className = 'product-detail-item';
+    //productBrand.innerText = 'Brand'
+    productInfo.appendChild(productBrand);
+    const productBrandTitle = document.createElement('h3');
+    productBrandTitle.className = 'product-item-title';
+    productBrandTitle.innerText = 'Brand'
+    productBrand.appendChild(productBrandTitle);
+    const productBrandText = document.createElement('p');
+    productBrandText.className = 'product-item-text';
+    productBrandText.innerText = `${item?.brand}`
+    productBrand.appendChild(productBrandText);
+
+    const productCategory = document.createElement('div');
+    productCategory.className = 'product-detail-item';
+    //productCategory.innerText = 'Category'
+    productInfo.appendChild(productCategory);
+    const productCategoryTitle = document.createElement('h3');
+    productCategoryTitle.className = 'product-item-title';
+    productCategoryTitle.innerText = 'Category'
+    productCategory.appendChild(productCategoryTitle);
+    const productCategoryText = document.createElement('p');
+    productCategoryText.className = 'product-item-text';
+    productCategoryText.innerText = `${item?.category}`
+    productCategory.appendChild(productCategoryText);
+
     const addBlock = document.createElement('div');
     addBlock.className = 'add-block';
     productData.appendChild(addBlock);
+    const cartButton = document.createElement('div');
+    cartButton.className = 'cart-button';
+    cartButton.innerText = `€ ${item?.price}`
+    addBlock.appendChild(cartButton);
+    const buttonAddRemove = document.createElement('button');
+    buttonAddRemove.className = 'btn-add-remove';
+    buttonAddRemove.innerText = 'ADD TO CART';
+    addBlock.appendChild(buttonAddRemove)
+    const buttonByu = document.createElement('button');
+    buttonByu.className = 'btn-buy-now';
+    buttonByu.innerText = 'BUY NOW';
+    addBlock.appendChild(buttonByu)
   }
 }

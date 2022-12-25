62f32c77677ccf2a8e9ffd2347289cb7ce6e7289
Stas Gatilov <stasgat12@gmail.com>
2022-12-25 23:16:19 +0300

    feat: add IProductData

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -9,3 +9,16 @@ export interface Coords {
   top: number;
   width: number;
 }
+export interface IProductData {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+}

91be04a737dff8305811151e64e8f6d3170f238d
Stas Gatilov <stasgat12@gmail.com>
2022-12-27 19:39:28 +0300

    fix: add additional conditions, remove commented code

--- src/pages/main/cards-block/sort.ts
+++ src/pages/main/cards-block/sort.ts
@@ -2,27 +2,24 @@ import { onlineStoreData } from '../../../data/data';
 import { IProductData } from '../../../interfaces/index';
 import { Filters } from '../filters-block/filters';
 import { CardsBlock } from './cards';
+import { Search } from './search';
 
 class CardsSort extends CardsBlock {
   filters: Filters;
   searchInput: HTMLInputElement | null;
   sortOptions: HTMLSelectElement | null;
+  search: Search;
 
   constructor(filters: Filters) {
     super();
     this.filters = filters;
+    this.search = new Search(filters);
     this.searchInput = document.querySelector('.search-bar__input');
     this.sortOptions = document.querySelector('.selector');
   }
 
   sort(): void {
     this.handleSelectedAttribute();
-    // this.sortByPriceAsc();
-    // this.sortByPriceDesc();
-    // this.sortByRatingAsc();
-    // this.sortByRatingDesc();
-    // this.sortByDiscountAsc();
-    // this.sortByDiscountDesc();
     if (this.sortOptions) {
       this.sortOptions.addEventListener('change', (): void => {
         this.sortByPriceAsc();
@@ -63,42 +60,60 @@ class CardsSort extends CardsBlock {
   sortByPriceAsc(): void {
     if (window.location.search.includes('sort=price-ASC')) {
       this.filters.products.sort((a, b) => a.price - b.price);
-      super.updateCatalog(this.filters.products);
+      super.notify(this.filters.products);
+      if (window.location.search.includes('search')) {
+        this.search.retainLastSearchAfterPageRefresh();
+      }
     }
   }
 
   sortByPriceDesc(): void {
     if (window.location.search.includes('sort=price-DESC')) {
       this.filters.products.sort((a, b) => b.price - a.price);
-      super.updateCatalog(this.filters.products);
+      super.notify(this.filters.products);
+      if (window.location.search.includes('search')) {
+        this.search.retainLastSearchAfterPageRefresh();
+      }
     }
   }
 
   sortByDiscountAsc(): void {
     if (window.location.search.includes('sort=discount-ASC')) {
       this.filters.products.sort((a, b) => a.discountPercentage - b.discountPercentage);
-      super.updateCatalog(this.filters.products);
+      super.notify(this.filters.products);
+      if (window.location.search.includes('search')) {
+        this.search.retainLastSearchAfterPageRefresh();
+      }
     }
   }
 
   sortByDiscountDesc(): void {
     if (window.location.search.includes('sort=discount-DESC')) {
       this.filters.products.sort((a, b) => b.discountPercentage - a.discountPercentage);
-      super.updateCatalog(this.filters.products);
+      super.notify(this.filters.products);
+      if (window.location.search.includes('search')) {
+        this.search.retainLastSearchAfterPageRefresh();
+      }
     }
   }
 
   sortByRatingAsc(): void {
     if (window.location.search.includes('sort=rating-ASC')) {
       this.filters.products.sort((a, b) => a.rating - b.rating);
-      super.updateCatalog(this.filters.products);
+      super.notify(this.filters.products);
+      if (window.location.search.includes('search')) {
+        this.search.retainLastSearchAfterPageRefresh();
+      }
     }
   }
 
   sortByRatingDesc(): void {
     if (window.location.search.includes('sort=rating-DESC')) {
       this.filters.products.sort((a, b) => b.rating - a.rating);
-      super.updateCatalog(this.filters.products);
+      super.notify(this.filters.products);
+      if (window.location.search.includes('search')) {
+        this.search.retainLastSearchAfterPageRefresh();
+      }
     }
   }
 }
